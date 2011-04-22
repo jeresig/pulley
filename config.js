989e5cbed49e9a1c18ee584468ac2a862bbe3a48
rwldrn <waldron.rick@gmail.com>
2011-04-22 11:03:13 -0400

    updates test config.js settings

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 {
 	// Use the follow to specify custom bug tracker URLs
 	repos: {
-		"jquery/jquery": "http://bugs.jquery.com/ticket/"
+		"rwldrn/sequence": ""
 	},
 	// You can specify these inline or in the Git config
 	// http://help.github.com/git-email-settings/
 	gitconfig: {
-		user: "", 
-		token: ""
+		user: "rwldrn", 
+		token: "a068b8bca039ef3bc38888aa996b3264"
 	}
 }
 
