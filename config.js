8f90d594bed0b2d97a9b28cee1c788229112bf86
rwldrn <waldron.rick@gmail.com>
2011-04-22 13:03:50 -0400

    Remove private token; prepare for pull request

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 {
 	// Use the follow to specify custom bug tracker URLs
 	repos: {
-		"rwldrn/sequence": false
+		"jquery/jquery": false
 	},
 	// You can specify these inline or in the Git config
 	// http://help.github.com/git-email-settings/
 	gitconfig: {
-		user: "rwldrn",
-		token: "a068b8bca039ef3bc38888aa996b3264"
+		user: "",
+		token: ""
 	}
 }
 
